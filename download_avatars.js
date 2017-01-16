0ed206fc0622d2ea8cbf6777385c26b9913b5d74
Hafiz Suara <hafbau@yahoo.com>
2017-01-16 21:37:35 +0000

    request url tested

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,16 @@
-var request = require('request');
+const request = require('request');
+
+const GITHUB_USER = 'hafbau';
+const GITHUB_TOKEN = '065146933e39bb482d8cbb1c426d23e81432d3ae';
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // create endpoint following GitHub API at https://developer.github.com/v3/repos/#list-contributors
-  let endpoint = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`; 
+  let endpoint = `@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  let requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}` + endpoint;
 
-  request.get(endpoint, function (err, response) {
+  request.get(requestURL, function (err, response) {
     cb(err, response);
   });
 }
