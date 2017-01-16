a5171344e69dc910247eaa845d03cecce2df044c
Hafiz Suara <hafbau@yahoo.com>
2017-01-16 21:29:38 +0000

    tested request with the call ok

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,21 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // create endpoint following GitHub API at https://developer.github.com/v3/repos/#list-contributors
+  let endpoint = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`; 
+
+  request.get(endpoint, function (err, response) {
+    cb(err, response);
+  });
+}
+
+
+
+
+// calling
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
